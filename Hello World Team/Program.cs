7f1080aa6e2b23e06cd2aff21747281d150748dd
omicron hex <omicronhex@outlook.com>
2021-03-04 11:59:24 -0500

    Added name and colour

--- Hello World Team/Program.cs
+++ Hello World Team/Program.cs
@@ -8,13 +8,12 @@ namespace Hello_World_Team
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I am on Team Orange");
-
+            Console.WriteLine("My name is Charlelz");
             Console.WriteLine("My name is Ryan");
             Console.WriteLine("My name is Tyler");
             Console.WriteLine("My name is Owen");
             Console.WriteLine("My name is Lucas");
             Console.WriteLine("My name is Leon");
-
         }
     }
 }
